552bc6a7f06143b9d773c16c01b923f6e48f39eb
surendra172001 <sap172001@gmail.com>
2020-09-09 14:26:49 +0530

    Added user schema

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
+if(process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
 var express= require('express')
 var http = require('http')
 var mongoose= require('mongoose')
 var body= require('body-parser')
+var {User} = require('./models/user');
 
 var app= express()
+
+const mongoURI = process.env.MONGODB_URI;
+
+
+
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+})
+.catch((err) => {
+    console.log(err);
+});
 
 app.use(express.static('public'))
 app.set("view engine", "ejs")
@@ -16,6 +35,24 @@ app.get("/login" , function(req, res){
 app.get("/register", function(req, res){
     res.render("register")
 })
+
+app.post('/login', function(req, res) {
+    console.log(req.body);
+});
+
+app.post('/register', function(req, res) {
+    // authentication 
+    console.log(req.body);
+    const newUser = new User(req.body);
+    User.create(newUser)
+    .then((doc) => {
+        console.log(doc);
+        res.send(doc);
+    })
+    .catch((err) => {
+        res.send(err);
+    })
+});
 
 app.listen(3040, "localhost", function () {
     console.log("Connected to server")
